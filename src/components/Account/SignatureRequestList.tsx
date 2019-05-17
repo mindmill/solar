f5d88bd50dbff58eb83e506ecbb9615d375277a5
Marcel Ebert <marcel@wolfgang-ebert.de>
2019-05-17 11:12:33 +0200

    Remove hoveractions from SignatureRequestListItems

--- src/components/Account/SignatureRequestList.tsx
+++ src/components/Account/SignatureRequestList.tsx
@@ -1,30 +1,22 @@
 import React from "react"
 import { Transaction } from "stellar-sdk"
-import Button from "@material-ui/core/Button"
 import ListSubheader from "@material-ui/core/ListSubheader"
-import Typography from "@material-ui/core/Typography"
-import ArrowForwardIcon from "@material-ui/icons/ArrowForward"
-import CloseIcon from "@material-ui/icons/Close"
 import { Account } from "../../context/accounts"
-import { SettingsContext } from "../../context/settings"
 import { SignatureRequest } from "../../lib/multisig-service"
 import { List } from "../List"
 import { TransactionListItem } from "./TransactionList"
-import { ActionButton, ConfirmDialog } from "../Dialog/Generic"
 import TransactionSender from "../TransactionSender"
 
 interface SignatureRequestListItemProps {
   accountPublicKey: string
   icon?: React.ReactElement<any>
-  onDismissSignatureRequest: (hash: string) => void
   onOpenTransaction?: (tx: Transaction, signatureRequest: SignatureRequest) => void
   signatureRequest: SignatureRequest
   style?: React.CSSProperties
 }
 
 function SignatureRequestListItem(props: SignatureRequestListItemProps) {
-  const { onDismissSignatureRequest, onOpenTransaction, signatureRequest } = props
-  const onDismiss = onDismissSignatureRequest ? () => onDismissSignatureRequest(signatureRequest.hash) : undefined
+  const { onOpenTransaction, signatureRequest } = props
   const onOpen = onOpenTransaction
     ? () => onOpenTransaction(signatureRequest.meta.transaction, signatureRequest)
     : undefined
@@ -34,19 +26,7 @@ function SignatureRequestListItem(props: SignatureRequestListItemProps) {
       accountPublicKey={signatureRequest.meta.transaction.source}
       createdAt={signatureRequest.created_at}
       icon={props.icon}
-      hoverActions={
-        <Typography component="div" color="textPrimary" style={{ display: "inline-flex", alignItems: "stretch" }}>
-          <Button onClick={onDismiss} color="inherit" variant="contained">
-            Dismiss&nbsp;
-            <CloseIcon style={{ fontSize: "140%" }} />
-          </Button>
-          <span style={{ display: "inline-block", width: 16 }} />
-          <Button onClick={onOpen} color="primary" variant="contained">
-            Review&nbsp;
-            <ArrowForwardIcon style={{ fontSize: "140%" }} />
-          </Button>
-        </Typography>
-      }
+      onOpenTransaction={onOpen}
       style={props.style}
       transaction={signatureRequest.meta.transaction}
     />
@@ -63,16 +43,6 @@ interface SignatureRequestListProps {
 
 // tslint:disable-next-line no-shadowed-variable
 export const SignatureRequestList = React.memo(function SignatureRequestList(props: SignatureRequestListProps) {
-  const settings = React.useContext(SettingsContext)
-  const [pendingConfirmation, setPendingConfirmation] = React.useState<SignatureRequest | null>(null)
-
-  const onConfirmDismissal = () => {
-    if (!pendingConfirmation) return
-
-    settings.ignoreSignatureRequest(pendingConfirmation.hash)
-    setPendingConfirmation(null)
-  }
-
   if (props.signatureRequests.length === 0) {
     return null
   }
@@ -87,7 +57,6 @@ export const SignatureRequestList = React.memo(function SignatureRequestList(pro
             key={signatureRequest.hash}
             accountPublicKey={props.accountPublicKey}
             icon={props.icon}
-            onDismissSignatureRequest={() => setPendingConfirmation(signatureRequest)}
             onOpenTransaction={props.onOpenTransaction}
             signatureRequest={signatureRequest}
             style={{
@@ -98,19 +67,6 @@ export const SignatureRequestList = React.memo(function SignatureRequestList(pro
           />
         ))}
       </List>
-      <ConfirmDialog
-        cancelButton={<ActionButton onClick={() => setPendingConfirmation(null)}>Cancel</ActionButton>}
-        confirmButton={
-          <ActionButton onClick={onConfirmDismissal} type="primary">
-            Confirm
-          </ActionButton>
-        }
-        onClose={() => setPendingConfirmation(null)}
-        open={pendingConfirmation !== null}
-        title="Confirm"
-      >
-        Dismiss pending multi-signature transaction?
-      </ConfirmDialog>
     </>
   )
 })
